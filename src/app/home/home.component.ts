17e7f22a1a2bd3dc3c5785a773d5c94432fe2ed2
Rodrigo R. Rubio Haro <chavo0022009@gmail.com>
2019-12-17 12:36:44 -0600

    V 1.0

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { faInfinity, faInfo, faBookOpen, faBrain} from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-
+import { Subject } from 'rxjs/internal/Subject';
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -25,7 +26,19 @@ export class HomeComponent implements OnInit {
   operationSystem = `+`;
   constructor() { }
 
-  ngOnInit() {
+  private _success = new Subject<string>();
+
+  staticAlertClosed = false;
+  successMessage: string;
+
+  ngOnInit(): void {
+    setTimeout(() => this.staticAlertClosed = true, 50000);
+
+    this._success.subscribe((message) => this.successMessage = message);
+    this._success.pipe(
+      debounceTime(5000)
+    ).subscribe(() => this.successMessage = null);
   }
+
 
 }
